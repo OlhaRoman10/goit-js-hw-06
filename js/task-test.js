df3b920285ed4ba608fbcdb0855a25e94f45eaed
Olha_Roman <ovgrozdova79@gmail.com>
2026-01-08 22:58:46 +0100

    class1

---
+++ js/task-test.js
@@ -0,0 +1,46 @@
+const pizzaPalace = {
+  pizzas: ["Supercheese", "Smoked", "Four meats"],
+  checkPizza(pizzaName) {
+    return this.pizzas.includes(pizzaName);
+  },
+  order(pizzaName) {
+    const isPizzaAvailable = this.checkPizza(pizzaName);
+
+    if (!isPizzaAvailable) {
+      return `Sorry, there is no pizza named «${pizzaName}»`;
+    }
+
+    return `Order accepted, preparing «${pizzaName}» pizza`;
+  },
+};
+
+
+const ancestor = {
+  name: "Paul",
+  age: 83,
+  surname: "Dawson",
+  heritage: "Irish",
+};
+
+const parent = Object.create(ancestor);
+parent.name = "Stacey";
+parent.surname = "Moore";
+parent.age = 54;
+
+const child = Object.create(parent);
+child.name = "Jason";
+child.age = 27;
+
+class User {
+  constructor(name, email) {
+    // Ініціалізація властивостей екземпляра
+    this.name = name;
+    this.email = email;
+  }
+}
+
+const mango = new User("Mango", "mango@mail.com");
+console.log(mango); // { name: 'Mango', email: 'mango@mail.com' }
+
+const poly = new User("Poly", "poly@mail.com");
+console.log(poly); // { name: 'Poly', email: 'poly@mail.com' }
